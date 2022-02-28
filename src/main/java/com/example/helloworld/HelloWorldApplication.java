cec01283565c42f26d5bdc5e5b02435c43fe31dd
Henry Tang <hktang@ualberta.ca>
2022-02-28 11:24:57 -0700

    Added static analysis stuff

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -9,6 +9,28 @@ import io.dropwizard.setup.Environment;
 public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
     public static void main(String[] args) throws Exception {
         new HelloWorldApplication().run(args);
+        if (args.length > 1) {
+            switch (args[0]) {
+                case "server":
+                    System.out.println("Working as a server...");
+                    break;
+                case "client":
+                    System.out.println("Working as a client...");
+                    break;
+            }
+        }
+    }
+
+    public static String camelCase(String input) {
+        StringBuilder sb = new StringBuilder();
+        for (String part : input.split("\\s+")) {
+            String tmp = part.toLowerCase();
+            char first = Character.toUpperCase(tmp.toCharArray()[0]);
+            char[] arr = tmp.toCharArray();
+            arr[0] = first;
+            sb.append(String.valueOf(arr));
+        }
+        return sb.toString();
     }
 
     @Override
